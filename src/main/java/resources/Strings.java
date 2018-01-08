a1c42a21bb5533daafdc9ef252fea5c6df7b5e1c
Deivdy <deivdysilva@ibict.br>
2018-01-08 14:10:01 -0200

    Layout da tela de login.

--- src/main/java/resources/Strings.java
+++ src/main/java/resources/Strings.java
@@ -9,7 +9,7 @@ public class Strings {
     //public static final String BASE = "http://tsicv.acv.ibict.br/sicv";
     //public static final String UPLOADED_FOLDER = "/opt/tomcat8/files/";
 
-    public static final String BASE = "http://localhost:8080/sicv";
-    public static final String UPLOADED_FOLDER = "";
+    public static final String BASE = "http://localhost:8080";
+    public static final String UPLOADED_FOLDER = "/home/deivdy/Desktop/files/";
 
 }
